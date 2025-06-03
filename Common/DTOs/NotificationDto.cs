08e06f3060bb8f940061d408f74a19e706f5620c
916 - Popa-Zelu-Andrei <applekiller733@gmail.com>
2025-06-03 22:26:24 +0300

    Deleted redundant DTOs

--- Common/DTOs/NotificationDto.cs
+++ Common/DTOs/NotificationDto.cs
@@ -12,38 +12,4 @@ namespace Common.DTOs
 
         public User UserReceiver { get; set; }
     }
-
-    public class FriendNotificationDto : NotificationDto
-    {
-        public User User { get; set; }
-
-        public User NewFriend { get; set; }
-
-        public User OldFriend { get; set; }
-    }
-
-    public class MessageNotificationDto : NotificationDto
-    {
-        public User MessageSender{ get; set; }
-
-        public int ChatId { get; set; }
-    }
-
-    public class TransactionNotificationDto : NotificationDto 
-    {
-        public User Receiver { get; set; }
-
-        public int ChatId { get; set; }
-
-        public string Type { get; set; }
-
-        public float Amount { get; set; }
-
-        public string Currency { get; set; }
-    }
-
-    public class NewChatNotificationDto : NotificationDto
-    {
-        public int ChatId { get; set; }
-    }
 }
