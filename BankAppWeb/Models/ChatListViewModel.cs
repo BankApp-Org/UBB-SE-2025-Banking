5b49d9032b480320801247e8149f2a85bc3b8ca9
George Roman <roman.geo@yahoo.com>
2025-06-05 20:02:18 +0300

    add: web part too

---
+++ BankAppWeb/Models/ChatListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Common.Models.Social;
+
+namespace BankAppWeb.Models
+{
+    public class ChatListViewModel
+    {
+        public string SearchQuery { get; set; } = string.Empty;
+        public List<Chat> ChatList { get; set; } = new List<Chat>();
+    }
+}
