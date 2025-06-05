98d04d053e27f808092e64780cab36f8e880111c
916-schiau-teodora <146817997+916-schiau-teodora@users.noreply.github.com>
2025-06-05 18:19:42 +0300

    initial commit, waiting for a method

---
+++ BankAppWeb/Models/CurrencyExchangeViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace LoanShark.MVC.Models
+{
+    public class CurrencyExchangeViewModel
+    {
+        public List<CurrencyExchangeRateDTO>? ExchangeRates { get; set; }
+    }
+
+    public class CurrencyExchangeRateDTO
+    {
+        public string FromCurrency { get; set; }
+        public string ToCurrency { get; set; }
+        public decimal ExchangeRate { get; set; }
+    }
+}
