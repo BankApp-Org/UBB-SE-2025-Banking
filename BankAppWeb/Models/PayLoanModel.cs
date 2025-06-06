e00e478298f2fc26ddfc70fc1037231c358b0f8e
916-Racz-Alexandra <alexandra.racz@stud.ubbcluj.ro>
2025-06-06 16:34:33 +0300

    pay loan web page added

---
+++ BankAppWeb/Models/PayLoanModel.cs
@@ -0,0 +1,17 @@
+﻿using Common.Models.Bank;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BankAppWeb.Models
+{
+    public class PayLoanModel
+    {
+        public int LoanId { get; set; }
+        public decimal Amount { get; set; }
+
+        public string SelectedBankAccountIban { get; set; }
+
+        public List<BankAccount> BankAccounts { get; set; }
+
+        public string PayErrorMessage;
+    }
+}
