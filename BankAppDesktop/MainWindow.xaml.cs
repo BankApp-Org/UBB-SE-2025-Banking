a88be93f24217ac0fec5c145e19285b7f123df7c
Pascu-Victor <me@vpascu.ro>
2025-06-07 00:58:26 +0300

    add tabs to desktop app

--- BankAppDesktop/MainWindow.xaml.cs
+++ BankAppDesktop/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 namespace BankAppDesktop
 {
     using BankAppDesktop.Pages;
+    using BankAppDesktop.Views;
     using BankAppDesktop.Views.Pages;
     using Common.Services;
     using Microsoft.Extensions.DependencyInjection;
@@ -8,7 +9,6 @@ namespace BankAppDesktop
     using Microsoft.UI.Xaml.Controls;
     using System;
     using System.ComponentModel;
-    using BankAppDesktop.Views;
 
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -62,6 +62,20 @@ namespace BankAppDesktop
             }
         }
 
+        private string _userName = "Guest";
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                if (_userName != value)
+                {
+                    _userName = value;
+                    OnPropertyChanged(nameof(UserName));
+                }
+            }
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
         public void OnPropertyChanged(string propertyName)
@@ -94,9 +108,22 @@ namespace BankAppDesktop
             this.IsUnauthenticated = !isLoggedIn;
             this.IsAuthenticated = isLoggedIn;
             this.IsAdmin = this.authenticationService.IsUserAdmin();
+
+            // Update the username
+            if (isLoggedIn)
+            {
+                var userSession = this.authenticationService.GetCurrentUserSession();
+                this.UserName = userSession?.UserName ?? "User";
+            }
+            else
+            {
+                this.UserName = "Guest";
+            }
+
             OnPropertyChanged(nameof(IsUnauthenticated));
             OnPropertyChanged(nameof(IsAuthenticated));
             OnPropertyChanged(nameof(IsAdmin));
+            OnPropertyChanged(nameof(UserName));
         }
 
         private void AuthenticationService_UserLoggedIn(object? sender, UserLoggedInEventArgs e)
@@ -115,32 +142,59 @@ namespace BankAppDesktop
         {
             if (args.SelectedItemContainer != null)
             {
-                string invokedItemTag = args.SelectedItemContainer.Tag.ToString() ?? throw new InvalidOperationException("Tag cannot be null");
-                this.MainFrame.Content = invokedItemTag switch
+                // Check if the selected item is a dropdown parent (has MenuItems collection)
+                if (args.SelectedItemContainer is NavigationViewItem navItem && navItem.MenuItems.Count > 0)
                 {
-                    "ChatReports" => this.serviceProvider.GetRequiredService<ChatReportPage>(),
-                    "LoanRequest" => this.serviceProvider.GetRequiredService<LoanRequestPage>(),
-                    "Loans" => this.serviceProvider.GetRequiredService<LoansPage>(),
-                    "UsersList" => this.serviceProvider.GetRequiredService<UsersPage>(),
-                    "BillSplitReports" => this.serviceProvider.GetRequiredService<BillSplitReportPage>(),
-                    "Investments" => this.serviceProvider.GetRequiredService<InvestmentsPage>(),
-                    "AlertsPage" => this.serviceProvider.GetRequiredService<AlertsPage>(),
-                    "AnalysisPage" => this.serviceProvider.GetRequiredService<AnalysisPage>(),
-                    "TipsPage" => this.serviceProvider.GetRequiredService<TipsPage>(),
-                    "HomePage" => this.serviceProvider.GetRequiredService<HomePage>(),
-                    "NewsListPage" => this.serviceProvider.GetRequiredService<NewsListPage>(),
-                    "CreateStockPage" => this.serviceProvider.GetRequiredService<CreateStockPage>(),
-                    "TransactionLogPage" => this.serviceProvider.GetRequiredService<TransactionLogPage>(),
-                    "ProfilePage" => this.serviceProvider.GetRequiredService<ProfilePage>(),
-                    "LoginPage" => this.serviceProvider.GetRequiredService<LoginPage>(),
-                    "GemStorePage" => this.serviceProvider.GetRequiredService<GemStorePage>(),
-                    "BankAccountCreateView" => this.serviceProvider.GetRequiredService<BankAccountCreateView>(),
-                    "NotificationsPage" => this.serviceProvider.GetRequiredService<NotificationsPage>(),
-                    "BankTransactionsHistoryPage" => this.serviceProvider.GetRequiredService<BankTransactionsHistoryPage>(),
-                    "MainPage" => this.serviceProvider.GetRequiredService<MainPage>(),
-                    _ => throw new InvalidOperationException($"Unknown navigation item: {invokedItemTag}")
-                };
+                    // This is a parent item with a dropdown - do nothing and let it just expand/collapse
+                    return;
+                }
+
+                // Check if the Tag property exists
+                if (args.SelectedItemContainer.Tag == null)
+                {
+                    // No Tag property, so don't try to navigate
+                    return;
+                }
+
+                string invokedItemTag = args.SelectedItemContainer.Tag.ToString() ?? throw new InvalidOperationException("Tag cannot be null");
+                NavigateToPage(invokedItemTag);
             }
+        }
+
+        private void UserMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is MenuFlyoutItem menuItem && menuItem.Tag is string tag)
+            {
+                NavigateToPage(tag);
+            }
+        }
+
+        private void NavigateToPage(string pageTag)
+        {
+            this.MainFrame.Content = pageTag switch
+            {
+                "ChatReports" => this.serviceProvider.GetRequiredService<ChatReportPage>(),
+                "LoanRequest" => this.serviceProvider.GetRequiredService<LoanRequestPage>(),
+                "Loans" => this.serviceProvider.GetRequiredService<LoansPage>(),
+                "UsersList" => this.serviceProvider.GetRequiredService<UsersPage>(),
+                "BillSplitReports" => this.serviceProvider.GetRequiredService<BillSplitReportPage>(),
+                "Investments" => this.serviceProvider.GetRequiredService<InvestmentsPage>(),
+                "AlertsPage" => this.serviceProvider.GetRequiredService<AlertsPage>(),
+                "AnalysisPage" => this.serviceProvider.GetRequiredService<AnalysisPage>(),
+                "TipsPage" => this.serviceProvider.GetRequiredService<TipsPage>(),
+                "HomePage" => this.serviceProvider.GetRequiredService<HomePage>(),
+                "NewsListPage" => this.serviceProvider.GetRequiredService<NewsListPage>(),
+                "CreateStockPage" => this.serviceProvider.GetRequiredService<CreateStockPage>(),
+                "TransactionLogPage" => this.serviceProvider.GetRequiredService<TransactionLogPage>(),
+                "ProfilePage" => this.serviceProvider.GetRequiredService<ProfilePage>(),
+                "LoginPage" => this.serviceProvider.GetRequiredService<LoginPage>(),
+                "GemStorePage" => this.serviceProvider.GetRequiredService<GemStorePage>(),
+                "BankAccountCreateView" => this.serviceProvider.GetRequiredService<BankAccountCreateView>(),
+                "NotificationsPage" => this.serviceProvider.GetRequiredService<NotificationsPage>(),
+                "BankTransactionsHistoryPage" => this.serviceProvider.GetRequiredService<BankTransactionsHistoryPage>(),
+                "MainPage" => this.serviceProvider.GetRequiredService<MainPage>(),
+                _ => throw new InvalidOperationException($"Unknown navigation item: {pageTag}")
+            };
         }
     }
 }
